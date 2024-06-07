30996a98a3dea7126c7640661dea19f46eca7092
George Holm <georgeholm@meta.com>
2024-06-07 14:29:30 -0700

    smallest changes

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("page loaded and ready for actions!");
+    loadReviews();
+
+  });
+
+function loadReviews() {
+    console.log("load reviews claled");
+}
+
+function handleReviewSubmit() {
+    console.log("handleReviewSubmit");
+}
